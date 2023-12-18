8993e8cef4256ed59e582aec982c81f8fe922806
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-18 17:56:15 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Validation from './components/Validation'
 
 function App() {
 
 
   return (
     <>
-    
+      <Validation />
     </>
   )
 }
